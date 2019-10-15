6375becce68033c00411b8ffbcd4088e8ef23fa9
drhaynes <drhmbp@gmail.com>
2019-10-15 14:36:18 +0100

    Initial Commit

---
+++ Trello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Trello
+//
+//  Created by David Haynes on 15/10/2019.
+//  Copyright © 2019 MBP Consulting Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
